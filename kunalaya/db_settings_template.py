b7ca7d6bfb8b1ad61f4bc31d6cff4b60b3ae8953
xKendoVul <hadyihernandez@gmail.com>
2025-09-16 16:04:09 -0600

    feat: conexion con postgresql y adicion de requirements

---
+++ kunalaya/db_settings_template.py
@@ -0,0 +1,10 @@
+DB = {
+  'default': {
+    'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    'NAME': '',
+    'USER': '',
+    'PASSWORD': '',
+    'HOST': 'localhost',
+    'PORT': '5432'
+  }
+}
--- kunalaya/settings.py
+++ kunalaya/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+from kunalaya.db_settings import DB
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -72,12 +73,7 @@ WSGI_APPLICATION = 'kunalaya.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = DB
 
 
 # Password validation
